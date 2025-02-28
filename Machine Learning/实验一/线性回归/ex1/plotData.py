a0de905fc6a81752c2e9290d6c41034693ec2d6e
Royce <roycelang24@gmail.com>
2025-02-28 16:54:57 +0800

    commit some files

---
+++ Machine Learning/实验一/线性回归/ex1/plotData.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def plotData(x, y):
+    """
+    绘制散点图
+    :param x:  x
+    :param y:  y
+    :return: None
+    """
+    pass
+    # ================ YOUR CODE HERE ================
+
